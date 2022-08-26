56bc04c83933f3c2524e07fe458e7d5c1bdf5bc6
ADMIN <ADMIN@LENOVO.mshome.net>
2022-08-26 19:36:07 +0530

    master branch

---
+++ Strings/src/strings/Removespecial.java
@@ -0,0 +1,16 @@
+package strings;
+
+public class Removespecial {
+	public static void main(String[]args) {
+		String R = "jvndbehcn**/***@$%%^&&&&*&*&*#^^^";
+		
+		R = R.replaceAll("[^a-zA-Z0-9]" ,"");
+		System.out.println("R");
+		
+				
+				
+				
+	}
+	
+
+}
---
+++ Strings/src/strings/Stringclass.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class Stringclass {
+public static void main (String[]args)
+{
+	String Ruturaj  = "200";
+	String Shreyas = "500";
+	String Shreeraj = "654";
+	String Harshada = "200";
+	System.out.println(Ruturaj);
+	System.out.println(Shreyas);
+	System.out.println (Harshada);
+	System.out.println (Shreeraj);
+	System.out.println("Ruturaj   "+"  Harshada");
+	 
+}
+}
